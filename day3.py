4ee3f3255fb141777ce47225e07d3776bbf08b54
Arianna Giannoccaro <123045858@upq.edu.mx>
2025-06-21 23:40:21 -0600

    updating

--- day3.py
+++ day3.py
@@ -13,10 +13,10 @@ print('Welcome to Treasure Island.\nYour mission is to find the treasure')
 print("You're at a cross road, Where do you want to go?")
 cross_road = input("Type 'left' or 'right':\n ").lower()
 
-if cross_road == 'left':
+if cross_road == 'left': # or 'Left'
     print("You've come to a lake. There is an island in the middle of the lake.")
     lake = input('Type "wait" to wait for a boat. "Swim" to swim across.\n').lower()
-    if lake == 'wait':
+    if lake == 'wait': #or 'Wait'
         print('You arrive at the island unharmed. There is a house with 3 doors.')
         color = input("Red, yellow and blue. Which colour do you choose?\n").lower()
         if color == 'yellow':
